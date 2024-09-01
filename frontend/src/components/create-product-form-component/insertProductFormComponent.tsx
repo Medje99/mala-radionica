16a29e8809c7a0ef708f26dc6909a053b98490be
vladimirhr988 <vladimirhr988@gmail.com>
2024-09-01 17:00:47 +0200

    insertProductForm Styled ready for modal

--- frontend/src/components/create-product-form-component/insertProductFormComponent.tsx
+++ frontend/src/components/create-product-form-component/insertProductFormComponent.tsx
@@ -1,10 +1,10 @@
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, Typography } from 'antd'
 import { IProducts } from '@/model/response/IProductResponse'
 import { useGlobalContext } from '@/contexts/GlobalContextProvider'
 import { useEffect } from 'react'
 import { onHandleSubmit } from './actions'
 
-const InsertProductFormComponent = () => {
+export const InsertProductFormComponent = () => {
   const { setHeaderTitle } = useGlobalContext()
   useEffect(() => {
     setHeaderTitle('Unos proizvoda')
@@ -12,31 +12,44 @@ const InsertProductFormComponent = () => {
   const [form] = Form.useForm<IProducts>()
 
   return (
-    <Form form={form} name="musterija-form" layout="vertical" className="bg-white p-5 rounded-lg width-2/3">
-      <Form.Item className="mb-4 w-1/2" label="Naziv proizvoda" text-center name="name">
-        <Input className="text-center"/>
-      </Form.Item>
-      <Form.Item className="mb-4 w-1/2" label="Proizvodjac" name="manufacturer">
+    <Form
+      form={form}
+      name="musterija-form"
+      title="Unos proizvoda"
+      layout="vertical"
+      className="bg-white p-10 rounded-lg w-2/5 mt-7 m-auto"
+    >
+      <Typography.Title level={2} className="text-center ">
+        Unos proizvoda
+      </Typography.Title>
+
+      <Form.Item className="mb-4 w-2/3 mt-7 m-auto" label="Naziv proizvoda" name="name">
         <Input className="text-center" />
       </Form.Item>
-      <Form.Item className="mb-4 w-1/2" label="Model" name="model">
-        <Input className="text-center"/>
-      </Form.Item>
-      <Form.Item className="mb-4 w-1/2" label="Cena" name="price">
-        <Input type="number" />
-      </Form.Item>
-      <Form.Item className="mb-4 w-1/2" label="Kolicina" name="quantity">
-        <Input type="number" />
-      </Form.Item>text-center
-      <Form.Item className="mb-4 w-1/2" label="SKU" name="SKU">
+      <Form.Item className="mb-4 w-2/3 m-auto" label="Proizvodjac" name="manufacturer">
         <Input className="text-center" />
       </Form.Item>
-      <Button type="primary" onClick={() => onHandleSubmit(form)}>
-        Potvrdi
-      </Button>
-      <Button type="default" onClick={() => form.resetFields()}>
-        Otkazi
-      </Button>
+      <Form.Item className="mb-4 w-2/3 m-auto" label="Model" name="model">
+        <Input className="text-center" />
+      </Form.Item>
+      <Form.Item className="mb-4 w-2/3 m-auto" label="Cena" name="price">
+        <Input type="number" />
+      </Form.Item>
+      <Form.Item className="mb-4 w-2/3 m-auto" label="Kolicina" name="quantity">
+        <Input type="number" />
+      </Form.Item>
+      <Form.Item className="mb-4 w-2/3 m-auto" label="SKU" name="SKU">
+        <Input className="text-center" />
+      </Form.Item>
+
+      <div className="flex justify-center gap-12 mt-5">
+        <Button type="primary" onClick={() => onHandleSubmit(form)}>
+          Potvrdi
+        </Button>
+        <Button type="default" onClick={() => form.resetFields()}>
+          Otkazi
+        </Button>
+      </div>
     </Form>
   )
 }
