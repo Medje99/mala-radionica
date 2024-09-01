e1a5b3501484e7fd947ac0190752df92be1b19f4
vladimirhr988 <vladimirhr988@gmail.com>
2024-09-01 16:27:51 +0200

    rendeer Home nav only on other pages

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,13 +10,18 @@ const Header = () => {
     setHeaderTitle(formTitleString)
   }, [formTitleString]) // removes lag
 
+  useEffect(() => {
+    
+  }, [location.pathname]); 
+
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center bg-black text-white header-container">
       <Typography className="text-2xl font-medium font-bold header-title">{headerTitle}</Typography>
       <div className="flex items-center gap-20 ml-auto main-nav">
-        <Link to="/" className="text-l font-medium hover:underline underline-offset-8 nav-link">
-          Pocetna
-        </Link>
+      {location.pathname !== '/' && ( 
+          <Link to="/" className="text-l font-medium hover:underline underline-offset-8 nav-link">
+            Pocetna
+          </Link>)}
         <Link to="/ProductCreate" className="text-l font-medium hover:underline underline-offset-8 nav-link">
           Novi proizvod
         </Link>
@@ -34,15 +39,15 @@ const Header = () => {
         </Link>
       </div>
       <nav className="ml-auto flex items-left gap-4 l:gap-10 external-links">
-        <Link
+        {/* <Link
           to="https://www.venerabike.rs/"
           className="text-l font-medium hover:underline underline-offset-8 external-link"
           target="_blank"
           rel="noopener noreferrer"
         >
           VeneraBike
-        </Link>
-        {/* ... other external links */}
+        </Link> */}
+  
       </nav>
     </header>
   )
--- frontend/src/components/create-product-form-component/insertProductFormComponent.tsx
+++ frontend/src/components/create-product-form-component/insertProductFormComponent.tsx
@@ -14,22 +14,22 @@ const InsertProductFormComponent = () => {
   return (
     <Form form={form} name="musterija-form" layout="vertical" className="bg-white p-5 rounded-lg width-2/3">
       <Form.Item className="mb-4 w-1/2" label="Naziv proizvoda" text-center name="name">
-        <Input />
+        <Input className="text-center"/>
       </Form.Item>
       <Form.Item className="mb-4 w-1/2" label="Proizvodjac" name="manufacturer">
         <Input className="text-center" />
       </Form.Item>
       <Form.Item className="mb-4 w-1/2" label="Model" name="model">
-        <Input />
+        <Input className="text-center"/>
       </Form.Item>
       <Form.Item className="mb-4 w-1/2" label="Cena" name="price">
         <Input type="number" />
       </Form.Item>
       <Form.Item className="mb-4 w-1/2" label="Kolicina" name="quantity">
         <Input type="number" />
-      </Form.Item>
+      </Form.Item>text-center
       <Form.Item className="mb-4 w-1/2" label="SKU" name="SKU">
-        <Input />
+        <Input className="text-center" />
       </Form.Item>
       <Button type="primary" onClick={() => onHandleSubmit(form)}>
         Potvrdi
