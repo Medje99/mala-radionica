2d4a3d5ab2bc80cc5606d2ab0ff66e2d6801cdf8
vladimirhr988 <vladimirhr988@gmail.com>
2024-09-07 16:55:50 +0200

    ibillresoponse updated to show updated Response

--- frontend/src/model/response/IBillResponse.ts
+++ frontend/src/model/response/IBillResponse.ts
@@ -1,19 +1,21 @@
 export interface IBillResponse {
-  job_description: string
-  firstName: string | null
   bill_id: number
-  id: number // The unique identifier for the bill
-  job_id: number // The ID of the job associated with the bill
-  end_date: Date // The end date of the job/bill, can be null
-  labor_cost: number // The cost of labor associated with the bill
-  paid: boolean // A flag indicating whether the bill has been paid
-  parts_cost?: number // The total cost of the bill, including labor and products used
-  // A string listing the products used (this could be an array or JSON if needed)
-  job_name: string // The name of the job associated with the bill
+  contact_id: number
+  job_id: number
+  end_date: Date
+  labor_cost: number
+  total_cost: number
+  paid: boolean
+  products_used: BillProducts[]
+  parts_cost: number
+  firstName: string
+  lastName: string
+  job_name: string
+  job_description: string
+  creation_date: Date
 }
 
 export interface BillProducts {
   product: IBillResponse
   quantity: number
 }
-;[]
